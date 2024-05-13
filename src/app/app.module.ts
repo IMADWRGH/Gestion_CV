ca5e5ef29b1f145ff0b5e6af9e0e4b839dcac97d
Imad Asserne <imad.asserne@gmail.com>
2024-05-13 14:09:51 +0000

    create component Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FeelContainerComponent } from './feel-container/feel-container.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FeelContainerComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
